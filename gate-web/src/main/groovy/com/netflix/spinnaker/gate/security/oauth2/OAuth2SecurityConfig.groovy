ff55760039b399d59ef472dbcfa85299e512def7
Jacob Kiefer <jacobkiefer@google.com>
2016-03-30 17:49:42 -0400

    Implemented PoC for Google OAuth2 provider, supported from Deck.

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/security/oauth2/OAuth2SecurityConfig.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/security/oauth2/OAuth2SecurityConfig.groovy
@@ -16,27 +16,49 @@
 
 package com.netflix.spinnaker.gate.security.oauth2
 
+import com.netflix.discovery.converters.Auto
 import com.netflix.spinnaker.gate.security.WebSecurityAugmentor
 import com.netflix.spinnaker.security.AuthenticatedRequest
 import com.netflix.spinnaker.security.User
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.http.converter.FormHttpMessageConverter
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.client.OAuth2ClientContext
+import org.springframework.security.oauth2.client.OAuth2RestTemplate
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices
 import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.authentication.RememberMeServices
+import org.springframework.security.web.authentication.logout.LogoutFilter
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.client.DefaultResponseErrorHandler
+import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 
@@ -44,12 +66,15 @@ import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Slf4j
 @CompileStatic
 @Configuration
 @ConditionalOnExpression('${oauth2.enabled:false}')
 class OAuth2SecurityConfig implements WebSecurityAugmentor {
+
   @Override
   void configure(HttpSecurity http, UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
     def filter = new OAuth2AuthenticationProcessingFilter() {
@@ -68,53 +93,80 @@ class OAuth2SecurityConfig implements WebSecurityAugmentor {
     http.addFilterBefore(filter, BasicAuthenticationFilter)
 
     http.csrf().disable()
+    http.authorizeRequests()
+      .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+      .antMatchers('/oauth/**').permitAll()
+      .antMatchers('/health').permitAll()
+      .antMatchers('/**').fullyAuthenticated()
   }
 
   @Override
   void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
     authenticationManagerBuilder.authenticationProvider(
-      authenticationProvider(identityResourceServerTokenServices(restTemplate()))
+      googleAuthenticationProvider(googleResourceServerTokenServices())
     )
   }
 
+  // Google specific stuff
   @Bean
-  AuthenticationProvider authenticationProvider(IdentityResourceServerTokenServices identityResourceServerTokenServices) {
-    return new OAuth2AuthenticationProvider(identityResourceServerTokenServices)
+  AuthenticationProvider googleAuthenticationProvider(ResourceServerTokenServices resourceServerTokenServices) {
+    return new GoogleOAuth2AuthenticationProvider(resourceServerTokenServices)
   }
 
   @Bean
-  IdentityResourceServerTokenServices identityResourceServerTokenServices(RestOperations restOperations) {
-    def defaultAccessTokenConverter = new DefaultAccessTokenConverter()
-    defaultAccessTokenConverter.userTokenConverter = new DefaultUserAuthenticationConverter()
+  ResourceServerTokenServices googleResourceServerTokenServices() {
+    new GoogleResourceServerTokenServices()
+  }
+  // End Google specific stuff
 
-    return new IdentityResourceServerTokenServices(
-      identityServerConfiguration(), restOperations, defaultAccessTokenConverter
-    )
+  // Netflix specific stuff
+//  @Bean
+//  AuthenticationProvider authenticationProvider(IdentityResourceServerTokenServices identityResourceServerTokenServices) {
+//    return new OAuth2AuthenticationProvider(identityResourceServerTokenServices)
+//  }
+//
+//  @Bean
+//  IdentityResourceServerTokenServices identityResourceServerTokenServices(RestOperations restOperations) {
+//    def defaultAccessTokenConverter = new DefaultAccessTokenConverter()
+//    defaultAccessTokenConverter.userTokenConverter = new DefaultUserAuthenticationConverter()
+//
+//    return new IdentityResourceServerTokenServices(
+//      identityServerConfiguration(), restOperations, defaultAccessTokenConverter
+//    )
+//  }
+//
+//  @Bean
+//  @ConfigurationProperties('oauth2')
+//  IdentityResourceServerTokenServices.IdentityServerConfiguration identityServerConfiguration() {
+//    new IdentityResourceServerTokenServices.IdentityServerConfiguration()
+//  }
+//
+//  static class DefaultUserAuthenticationConverter implements UserAuthenticationConverter {
+//    @Override
+//    Map<String, ?> convertUserAuthentication(Authentication userAuthentication) {
+//      return [:]
+//    }
+//
+//    @Override
+//    Authentication extractAuthentication(Map<String, ?> map) {
+//      def allowedAccounts = (map.scope ?: []).collect { String scope -> scope.replace("spinnaker_", "") }
+//      def user = new User(map.client_id as String, null, null, [], allowedAccounts)
+//      return new UsernamePasswordAuthenticationToken(user, "N/A", [])
+//    }
+//  }
+  // End Netflix specific stuff
+
+  @Bean
+  @ConditionalOnMissingBean(RestTemplate)
+  RestTemplate restTemplate() {
+    def template = new RestTemplate()
+    template.getMessageConverters().add(new FormHttpMessageConverter())
+    return template
   }
 
   @Bean
-  @ConfigurationProperties('oauth2')
-  IdentityResourceServerTokenServices.IdentityServerConfiguration identityServerConfiguration() {
-    new IdentityResourceServerTokenServices.IdentityServerConfiguration()
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(RestOperations)
-  RestOperations restTemplate() {
-    new RestTemplate()
-  }
-
-  static class DefaultUserAuthenticationConverter implements UserAuthenticationConverter {
-    @Override
-    Map<String, ?> convertUserAuthentication(Authentication userAuthentication) {
-      return [:]
-    }
-
-    @Override
-    Authentication extractAuthentication(Map<String, ?> map) {
-      def allowedAccounts = (map.scope ?: []).collect { String scope -> scope.replace("spinnaker_", "")}
-      def user = new User(map.client_id as String, null, null, [], allowedAccounts)
-      return new UsernamePasswordAuthenticationToken(user, "N/A", [])
-    }
+  @ConfigurationProperties('spring.oauth2.client')
+  protected OAuth2ProtectedResourceDetails resource() {
+    new AuthorizationCodeResourceDetails()
   }
 }
