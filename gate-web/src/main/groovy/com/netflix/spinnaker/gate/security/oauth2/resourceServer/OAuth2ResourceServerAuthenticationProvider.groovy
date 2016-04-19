65f257c281654bb6e76bc576658f19452419cf91
Travis Tomsu <ttomsu@google.com>
2016-04-19 15:56:57 -0400

    Adds generic AuthController and organizes a bunch of classes (#191)
    * Adds generic AuthController and organizes a bunch of classes
    
    * Moves AuthController into controllers package

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/security/oauth2/resourceServer/OAuth2ResourceServerAuthenticationProvider.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/security/oauth2/resourceServer/OAuth2ResourceServerAuthenticationProvider.groovy
@@ -1,11 +1,11 @@
 /*
- * Copyright 2015 Netflix, Inc.
+ * Copyright 2016 Netflix, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,19 +14,18 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.gate.security.oauth2
+package com.netflix.spinnaker.gate.security.oauth2.resourceServer
 
-import com.netflix.spinnaker.gate.security.oauth2.IdentityResourceServerTokenServices
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 
-class OAuth2AuthenticationProvider implements AuthenticationProvider {
+class OAuth2ResourceServerAuthenticationProvider implements AuthenticationProvider {
   private final IdentityResourceServerTokenServices identityResourceServerTokenServices
 
-  OAuth2AuthenticationProvider(IdentityResourceServerTokenServices identityResourceServerTokenServices) {
+  OAuth2ResourceServerAuthenticationProvider(IdentityResourceServerTokenServices identityResourceServerTokenServices) {
     this.identityResourceServerTokenServices = identityResourceServerTokenServices
   }
 
