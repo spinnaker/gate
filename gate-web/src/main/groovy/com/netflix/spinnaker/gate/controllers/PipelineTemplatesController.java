b1891b4b636a18b47eb96e11a87784eb96daa9cf
Rob Zienert <rzienert@netflix.com>
2017-09-20 11:40:04 -0700

    feat(pipeline_template): Convert pipeline config to pipeline template

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineTemplatesController.java
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineTemplatesController.java
@@ -22,7 +22,6 @@ import com.netflix.spinnaker.gate.services.PipelineTemplateService;
 import com.netflix.spinnaker.gate.services.TaskService;
 import com.netflix.spinnaker.security.AuthenticatedRequest;
 import io.swagger.annotations.ApiOperation;
-import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +33,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
