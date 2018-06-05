6c17724e38b9ef35f9bcbcd5a36be944ada1184f
Joon Lim <joonlim@google.com>
2018-06-05 10:29:41 -0400

    feat(triggers): Add endpoint to search for pipeline executions and change webhooks endpoint to return a map.

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/ExecutionsController.java
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/ExecutionsController.java
@@ -15,18 +15,17 @@
  */
 package com.netflix.spinnaker.gate.controllers;
 
-import java.util.List;
-
 import com.netflix.spinnaker.gate.security.RequestContext;
 import com.netflix.spinnaker.gate.services.internal.OrcaServiceSelector;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/executions")
 @RestController
 public class ExecutionsController {
 
@@ -38,10 +37,29 @@ public class ExecutionsController {
   }
 
   @ApiOperation(value = "Retrieve a list of the most recent pipeline executions for the provided `pipelineConfigIds` that match the provided `statuses` query parameter")
-  @RequestMapping(method = RequestMethod.GET)
+  @RequestMapping(value = "/executions", method = RequestMethod.GET)
   List getLatestExecutionsByConfigIds(@RequestParam(value = "pipelineConfigIds") String pipelineConfigIds,
                                       @RequestParam(value = "limit", required = false) Integer limit,
                                       @RequestParam(value = "statuses", required = false) String statuses) {
     return orcaServiceSelector.withContext(RequestContext.get()).getLatestExecutionsByConfigIds(pipelineConfigIds, limit, statuses);
+  }
+
+  @ApiOperation(value = "Search for pipeline executions using a combination of criteria")
+  @RequestMapping(value = "/applications/{application}/executions/{triggerType}", method = RequestMethod.GET)
+  List searchForPipelineExecutions(
+    @PathVariable(value = "application") String application,
+    @PathVariable(value = "triggerType") String triggerType,
+    @RequestParam(value = "pipelineName", required = false) String pipelineName,
+    @RequestParam(value = "eventId", required = false) String eventId,
+    @RequestParam(value = "trigger", required = false) String trigger,
+    @RequestParam(value = "triggerTimeStartBoundary", defaultValue = "0") long triggerTimeStartBoundary,
+    @RequestParam(value = "triggerTimeEndBoundary", defaultValue = "9223372036854775807" /* Long.MAX_VALUE */) long triggerTimeEndBoundary,
+    @RequestParam(value = "statuses", required = false) String statuses,
+    @RequestParam(value = "startIndex", defaultValue =  "0") int startIndex,
+    @RequestParam(value = "size", defaultValue = "10") int size,
+    @RequestParam(value = "reverse", defaultValue = "false") boolean reverse,
+    @RequestParam(value = "expand", defaultValue = "false") boolean expand
+  ) {
+    return orcaServiceSelector.withContext(RequestContext.get()).searchForPipelineExecutions(application, triggerType, pipelineName, eventId, trigger, triggerTimeStartBoundary, triggerTimeEndBoundary, statuses, startIndex, size, reverse, expand);
   }
 }
