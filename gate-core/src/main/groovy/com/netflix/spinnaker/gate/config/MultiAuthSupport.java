73369a5a014b93d2fd33eef981b9251e553f779c
Nicolas Cohen <nicolas.cohen@armory.io>
2019-07-02 13:25:35 -0700

    fix(auth): Apply auth filters to all connectors except API port (x509)

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/MultiAuthSupport.java
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/MultiAuthSupport.java
@@ -1,41 +1,22 @@
 package com.netflix.spinnaker.gate.config;
 
-import org.springframework.boot.web.context.WebServerApplicationContext;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class MultiAuthSupport {
 
+  @Value("${default.apiPort:0}")
+  private int apiPort;
+
   @Bean
-  RequestMatcherProvider multiAuthRequestMatcherProvider(ApplicationContext applicationContext) {
+  RequestMatcherProvider multiAuthRequestMatcherProvider() {
     return new RequestMatcherProvider() {
       @Override
       public RequestMatcher requestMatcher() {
-        if (applicationContext instanceof WebServerApplicationContext) {
-          final WebServerApplicationContext ctx = (WebServerApplicationContext) applicationContext;
-          return req -> {
-            // we have to do this per request because at bean-creation time the WebServer has not
-            // yet been created
-            final WebServer webServer = ctx.getWebServer();
-            if (webServer instanceof TomcatWebServer) {
-              if (((TomcatWebServer) webServer).getTomcat().getService().findConnectors().length
-                  > 1) {
-                final int localPort = req.getLocalPort();
-                final int defaultPort = webServer.getPort();
-                return localPort == defaultPort;
-              }
-            }
-            return true;
-          };
-        }
-
-        return AnyRequestMatcher.INSTANCE;
+        return req -> req.getLocalPort() != apiPort;
       }
     };
   }
