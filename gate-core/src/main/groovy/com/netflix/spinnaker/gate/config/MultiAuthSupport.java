a54c2d1ee0752c053840a0806b58c7b0f7f597d6
Nicolas Cohen <nicolas.cohen@armory.io>
2019-07-02 15:52:14 -0700

    fix(auth): Add legacyServerPortAuth setting to enable auth on legacy port

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/MultiAuthSupport.java
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/MultiAuthSupport.java
@@ -1,22 +1,52 @@
 package com.netflix.spinnaker.gate.config;
 
+import com.netflix.spinnaker.config.TomcatConfigurationProperties;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class MultiAuthSupport {
 
-  @Value("${default.apiPort:0}")
-  private int apiPort;
+  @Value("${default.legacy-server-port-auth:true}")
+  private boolean legacyServerPortAuth;
 
   @Bean
-  RequestMatcherProvider multiAuthRequestMatcherProvider() {
+  RequestMatcherProvider multiAuthRequestMatcherProvider(
+      ApplicationContext applicationContext,
+      TomcatConfigurationProperties tomcatConfigurationProperties) {
     return new RequestMatcherProvider() {
       @Override
       public RequestMatcher requestMatcher() {
-        return req -> req.getLocalPort() != apiPort;
+        if (applicationContext instanceof WebServerApplicationContext) {
+          final WebServerApplicationContext ctx = (WebServerApplicationContext) applicationContext;
+          return req -> {
+            if (legacyServerPortAuth
+                && tomcatConfigurationProperties.getLegacyServerPort() == req.getLocalPort()) {
+              return true;
+            }
+            // we have to do this per request because at bean-creation time the WebServer has not
+            // yet been created
+            final WebServer webServer = ctx.getWebServer();
+            if (webServer instanceof TomcatWebServer) {
+              if (((TomcatWebServer) webServer).getTomcat().getService().findConnectors().length
+                  > 1) {
+                final int localPort = req.getLocalPort();
+                final int defaultPort = webServer.getPort();
+                return localPort == defaultPort;
+              }
+            }
+            return true;
+          };
+        }
+
+        return AnyRequestMatcher.INSTANCE;
       }
     };
   }
