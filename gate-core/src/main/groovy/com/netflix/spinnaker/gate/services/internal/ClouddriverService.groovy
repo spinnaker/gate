a5488dff3f5bf01a2ce885c7b07a1276fc593ca9
Deharam <dmadhu@8c85902e4110.ant.amazon.com>
2019-07-09 17:29:16 +0530

    feat(provider/aws): Added a new API, to get the lambda functions.

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/services/internal/ClouddriverService.groovy
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/services/internal/ClouddriverService.groovy
@@ -383,4 +383,9 @@ interface ClouddriverService {
                          @Query(value = "region") String region,
                          @Query(value = "serviceInstanceName") String serviceInstanceName)
 
+  @GET(value = "/functions")
+  List<Map> getFunctions(@Query(value = "functionName") String functionName,
+                         @Query(value = "region") String region,
+                         @Query(value = "account") String account)
+
 }
