4444b52be19d838e4ad7738453c37ebd6e3a8a5b
chris smalley <csmalley@netflix.com>
2020-01-27 10:53:29 -0800

    fix(plugins): Set spring.applicaiton.name test property in MainSpec

--- gate-web/src/test/groovy/com/netflix/spinnaker/gate/MainSpec.java
+++ gate-web/src/test/groovy/com/netflix/spinnaker/gate/MainSpec.java
@@ -8,7 +8,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {Main.class})
-@TestPropertySource(properties = {"spring.config.location=classpath:gate-test.yml"})
+@TestPropertySource(
+    properties = {"spring.config.location=classpath:gate-test.yml", "spring.application.name=gate"})
 public class MainSpec {
   @Test
   public void startupTest() {}
