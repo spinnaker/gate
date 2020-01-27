883ea37104f9c29ea24440f83990fd865126185b
chris smalley <csmalley@netflix.com>
2020-01-27 11:09:23 -0800

    fix(plugins): Add property to gate-test.yml and some failing specs

--- gate-web/src/test/groovy/com/netflix/spinnaker/gate/MainSpec.java
+++ gate-web/src/test/groovy/com/netflix/spinnaker/gate/MainSpec.java
@@ -8,8 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {Main.class})
-@TestPropertySource(
-    properties = {"spring.config.location=classpath:gate-test.yml", "spring.application.name=gate"})
+@TestPropertySource(properties = {"spring.config.location=classpath:gate-test.yml"})
 public class MainSpec {
   @Test
   public void startupTest() {}
