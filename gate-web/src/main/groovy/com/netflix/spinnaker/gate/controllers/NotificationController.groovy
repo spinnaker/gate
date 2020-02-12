d65faa3277f68d4b07292c8a0428149a59eba9f8
Luis Pollo <lpollo@netflix.com>
2020-02-12 12:32:04 -0800

    Address review feedback

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/NotificationController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/NotificationController.groovy
@@ -24,6 +24,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -59,9 +60,8 @@ class NotificationController {
    * @param request The callback request
    * @return
    */
-  @RequestMapping(
+  @PostMapping(
     value = '/callbacks/{source}',
-    method = RequestMethod.POST,
     consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE
   )
