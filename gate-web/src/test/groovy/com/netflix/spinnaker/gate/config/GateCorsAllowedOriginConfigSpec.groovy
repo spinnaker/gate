6a2abdc26daf2925e4a7be914758c58efba73529
Sairam Rekapalli <srekapalli@netflix.com>
2020-08-13 20:27:42 -0700

    fix(misc): fix config

--- gate-web/src/test/groovy/com/netflix/spinnaker/gate/config/GateCorsAllowedOriginConfigSpec.groovy
+++ gate-web/src/test/groovy/com/netflix/spinnaker/gate/config/GateCorsAllowedOriginConfigSpec.groovy
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureServiceClientProvider
 @SpringBootTest(classes = Main)
 @ActiveProfiles('alloworigincors')
-@TestPropertySource(properties = ["spring.config.location=classpath:gate-test.yml"])
+@TestPropertySource(properties = ["spring.config.location=classpath:gate-test.yml", "retrofit.enabled=true"])
 class GateCorsAllowedOriginConfigSpec extends Specification {
 
   @Autowired
