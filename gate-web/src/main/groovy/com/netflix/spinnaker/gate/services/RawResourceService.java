79dda3c224b2fb11f488fc4d3287b9af9297f917
Julien Duchesne <jduchesne@coveo.com>
2020-10-28 13:18:21 -0400

    feat(web): Add the rawResources clouddriver endpoint
    It serves all resources regardless of their Spinnaker kind

---
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/services/RawResourceService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Coveo, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.spinnaker.gate.services;
+
+import com.netflix.spinnaker.gate.services.internal.ClouddriverServiceSelector;
+import groovy.transform.CompileStatic;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@CompileStatic
+@Component
+public class RawResourceService {
+
+  @Autowired private ClouddriverServiceSelector clouddriverServiceSelector;
+
+  public List<Map> getApplicationRawResources(String appName, String selectorKey) {
+    return clouddriverServiceSelector.select().getApplicationRawResources(appName);
+  }
+}
