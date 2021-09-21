498e4efd4522c9dcaa22b4dd0a85cb03010174b3
Pranav-b-7 <pranav.bhaskaran@opsmx.io>
2021-09-21 20:57:12 +0530

    Added enableFeignClient annotation

--- gate-web/src/main/java/com/netflix/spinnaker/gate/config/GateCorsConfig.java
+++ gate-web/src/main/java/com/netflix/spinnaker/gate/config/GateCorsConfig.java
@@ -24,6 +24,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -31,6 +32,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableFeignClients
 public class GateCorsConfig {
 
   private static final List<String> ALLOWED_HEADERS =
