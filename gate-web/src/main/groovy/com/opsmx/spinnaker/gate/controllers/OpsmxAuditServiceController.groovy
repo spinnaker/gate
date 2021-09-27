f9a817f03547574b383abfb27bb15ff58abcd572
Pranav-b-7 <pranav.bhaskaran@opsmx.io>
2021-09-27 18:53:38 +0530

    Bypass the authentication for spinnaker webhook url.

---
+++ gate-web/src/main/groovy/com/opsmx/spinnaker/gate/controllers/OpsmxAuditServiceController.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.opsmx.spinnaker.gate.controllers
+
+import com.opsmx.spinnaker.gate.services.OpsmxAuditService
+import groovy.util.logging.Slf4j
+import io.swagger.annotations.ApiOperation
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@RestController
+@RequestMapping("/auditservice")
+@ConditionalOnExpression('${services.auditservice.enabled:false}')
+class OpsmxAuditServiceController {
+
+  @Autowired
+  OpsmxAuditService opsmxAuditService
+
+  @ApiOperation(value = "Endpoint for audit-client rest services")
+  @RequestMapping(value = "/{version}/{type}/{source}/{source1}", method = RequestMethod.POST)
+  Object getAuditClientResponse3(@PathVariable("version") String version,
+                                 @PathVariable("type") String type,
+                                 @PathVariable("source") String source,
+                                 @PathVariable("source1") String source1,
+                                 @RequestBody(required = false) Object data) {
+
+    return opsmxAuditService.postAuditService1(version, type, source, source1, data)
+  }
+
+}
