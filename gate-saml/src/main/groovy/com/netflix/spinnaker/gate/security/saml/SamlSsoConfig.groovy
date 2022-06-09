c9aeffe974d950a3c14bcd12ec7f137a7d73e3f6
Rahul Chekuri <89373036+rahul-chekuri@users.noreply.github.com>
2022-06-09 15:12:39 +0530

    OP-14834: invalidate session and throw SAMLAuthenticationException (#209) (#216)
    * OP-14834: Check and update the token.
    
    * OP-14834: Updated logs and packages.
    
    * OP-14834: Only validates ExpiringUsernameAuthenticationToken.
    
    * OP-14834: Only validates ExpiringUsernameAuthenticationToken.
    
    * OP-14834: Added SamlAuthTokenUpdateFilter to the filter chain.
    
    * OP-14834: Added SamlAuthTokenUpdateFilter to the filter chain after basic auth filter.
    
    * OP-14834: redirect to logout
    
    * OP-14834: throw AccessDeniedException
    
    * OP-14834:redirect to /saml/login.
    
    * OP-14834:redirect to /saml/sso.
    
    * OP-14834: send html response
    
    * OP-14834: clear context
    
    * OP-14834: invalidate session
    
    * OP-14834: and then logout
    
    * OP-14834: clear context, invalidate session and redirect to log out url.
    
    * OP-14834: invalidate session and throw SAMLAuthenticationException
    
    * OP-14834: removed commented code.
    
    * OP-14834: Updated the log message.
    
    * OP-14834: Updated the certificate

--- gate-saml/src/main/groovy/com/netflix/spinnaker/gate/security/saml/SamlSsoConfig.groovy
+++ gate-saml/src/main/groovy/com/netflix/spinnaker/gate/security/saml/SamlSsoConfig.groovy
@@ -24,6 +24,7 @@ import com.netflix.spinnaker.gate.security.SpinnakerAuthConfig
 import com.netflix.spinnaker.gate.services.PermissionService
 import com.netflix.spinnaker.kork.core.RetrySupport
 import com.netflix.spinnaker.security.User
+import com.opsmx.spinnaker.gate.security.saml.SamlAuthTokenUpdateFilter
 import groovy.util.logging.Slf4j
 import org.opensaml.saml2.core.Assertion
 import org.opensaml.saml2.core.Attribute
@@ -43,11 +44,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.extensions.saml2.config.SAMLConfigurer
-import org.springframework.security.saml.websso.WebSSOProfileConsumerImpl
 import org.springframework.security.saml.SAMLCredential
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService
+import org.springframework.security.saml.websso.WebSSOProfileConsumerImpl
 import org.springframework.security.web.authentication.RememberMeServices
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.session.web.http.DefaultCookieSerializer
 import org.springframework.stereotype.Component
 
@@ -168,7 +170,9 @@ class SamlSsoConfig extends WebSecurityConfigurerAdapter {
           .keyPassword(samlSecurityConfigProperties.keyStorePassword)
 
       saml.init(http)
-
+    SamlAuthTokenUpdateFilter authTokenUpdateFilter = new SamlAuthTokenUpdateFilter()
+    http.addFilterAfter(authTokenUpdateFilter,
+        BasicAuthenticationFilter.class)
     // @formatter:on
 
   }
