131832d697cb4c450ba8f4595b62b847a1551466
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2022-08-08 17:08:51 +0530

    OP-16541: Added contentSecurityPolicy header.

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/AuthConfig.groovy
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/AuthConfig.groovy
@@ -94,6 +94,9 @@ class AuthConfig {
   @Value('${allowUnauthenticatedAccess.webhooks:false}')
   boolean isSpinnakerWebhooksUnauthenticatedAccessEnabled
 
+  @Value('${security.contentSecurityPolicy:\'object-src \'none\'; script-src \'unsafe-eval\' \'unsafe-inline\' https: http:;\'}')
+  String contentSecurityPolicy
+
   void configure(HttpSecurity http) throws Exception {
     // @formatter:off
     if(isAgentAPIUnauthenticatedAccessEnabled && isSpinnakerWebhooksUnauthenticatedAccessEnabled){
@@ -329,6 +332,8 @@ class AuthConfig {
     if (webhookDefaultAuthEnabled) {
       http.authorizeRequests().antMatchers(HttpMethod.POST, '/webhooks/**').authenticated()
     }
+
+    http.headers().contentSecurityPolicy(contentSecurityPolicy)
 
     http.logout()
         .logoutUrl("/auth/logout")
