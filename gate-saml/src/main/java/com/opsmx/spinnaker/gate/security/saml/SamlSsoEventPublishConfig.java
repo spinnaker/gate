97a4a448baad62697d60cd7122726e081da6a0bc
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2022-08-10 06:21:48 +0530

    OP-17106: Added SamlSsoEventPublishConfig to update ApplicationEventPublisher.

---
+++ gate-saml/src/main/java/com/opsmx/spinnaker/gate/security/saml/SamlSsoEventPublishConfig.java
@@ -0,0 +1,57 @@
+package com.opsmx.spinnaker.gate.security.saml;
+
+import java.util.List;
+import javax.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.saml.SAMLProcessingFilter;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@ConditionalOnExpression("${saml.enabled:false}")
+@Configuration
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SamlSsoEventPublishConfig {
+
+  private ApplicationEventPublisher applicationEventPublisher;
+
+  @Autowired
+  @Qualifier("springSecurityFilterChain")
+  private Filter springSecurityFilterChain;
+
+  @Autowired
+  public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+    this.applicationEventPublisher = applicationEventPublisher;
+    log.info("successfully set applicationEventPublisher");
+  }
+
+  @Bean
+  public FilterChainProxy getFilters() {
+    log.info("The applicationEventPublisher is: " + applicationEventPublisher.getClass());
+    FilterChainProxy filterChainProxy = (FilterChainProxy) springSecurityFilterChain;
+    List<SecurityFilterChain> list = filterChainProxy.getFilterChains();
+    list.stream()
+        .flatMap(chain -> chain.getFilters().stream())
+        .forEach(filter -> log.info("The filter is: " + filter.getClass()));
+
+    list.stream()
+        .flatMap(chain -> chain.getFilters().stream())
+        .filter(filter -> filter.getClass() == SAMLProcessingFilter.class)
+        .findAny()
+        .map(SAMLProcessingFilter.class::cast)
+        .ifPresent(
+            filter -> {
+              log.info("The SAMLProcessingFilter is: " + filter.getClass());
+              filter.setApplicationEventPublisher(applicationEventPublisher);
+            });
+    return filterChainProxy;
+  }
+}
