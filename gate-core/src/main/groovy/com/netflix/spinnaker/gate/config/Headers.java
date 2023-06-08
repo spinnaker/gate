2530a0870bbc68c305a61c5350c2be59852e9d4a
Matt Sicker <msicker@apple.com>
2023-06-08 15:41:13 -0500

    refactor(core): Rewrite Headers in Java

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/Headers.java
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/config/Headers.java
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.gate.config
+package com.netflix.spinnaker.gate.config;
 
-class Headers {
-  public static final String AUTHENTICATION_REDIRECT_HEADER_NAME = "X-AUTH-REDIRECT-URL"
+public class Headers {
+  public static final String AUTHENTICATION_REDIRECT_HEADER_NAME = "X-AUTH-REDIRECT-URL";
 }
