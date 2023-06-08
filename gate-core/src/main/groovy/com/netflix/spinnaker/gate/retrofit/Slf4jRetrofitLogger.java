a0f7c67a24663010a04ed827758db22b47fab0c3
Matt Sicker <msicker@apple.com>
2023-06-08 15:40:38 -0500

    refactor(core): Rewrite Slf4jRetrofitLogger in Java

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/retrofit/Slf4jRetrofitLogger.java
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/retrofit/Slf4jRetrofitLogger.java
@@ -14,25 +14,25 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.gate.retrofit
+package com.netflix.spinnaker.gate.retrofit;
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import retrofit.RestAdapter
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit.RestAdapter;
 
-class Slf4jRetrofitLogger implements RestAdapter.Log {
-    private final Logger logger
+public class Slf4jRetrofitLogger implements RestAdapter.Log {
+  public Slf4jRetrofitLogger(Class<?> type) {
+    this(LoggerFactory.getLogger(type));
+  }
 
-    public Slf4jRetrofitLogger(Class type) {
-        this(LoggerFactory.getLogger(type))
-    }
+  public Slf4jRetrofitLogger(Logger logger) {
+    this.logger = logger;
+  }
 
-    public Slf4jRetrofitLogger(Logger logger) {
-        this.logger = logger
-    }
+  @Override
+  public void log(String message) {
+    logger.info(message);
+  }
 
-    @Override
-    void log(String message) {
-        logger.info(message)
-    }
+  private final Logger logger;
 }
