091123ed3666275ff359500a1c890cda7ed0740f
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-08-08 14:43:11 +0100

    fix: Placing the CloudEventHandlerConfiguration in gate-web project

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/config/CloudEventHandlerConfiguration.java
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/config/CloudEventHandlerConfiguration.java
@@ -16,18 +16,19 @@
 package com.netflix.spinnaker.gate.config;
 
 import io.cloudevents.spring.mvc.CloudEventHttpMessageConverter;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class CloudEventHandlerConfiguration implements WebMvcConfigurer {
 
   @Override
   public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    converters.add(0, cloudEventHttpMessageConverter());
+    converters.add(cloudEventHttpMessageConverter());
   }
 
   @Bean
