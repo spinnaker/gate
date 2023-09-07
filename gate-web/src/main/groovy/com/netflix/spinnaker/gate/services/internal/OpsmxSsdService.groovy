7954bdc5aefbf438e4e3269d61c26aed3dad455b
vivek-opsmx <vivek.k@opsmx.io>
2023-09-07 16:37:33 +0530

    Added search param to the Gate Url

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/services/internal/OpsmxSsdService.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/services/internal/OpsmxSsdService.groovy
@@ -86,6 +86,7 @@ interface OpsmxSsdService {
                          @Query("pageNo") Integer pageNo,
                          @Query("pageLimit") Integer pageLimit,
                          @Query("kind") String kind,
+                         @Query("search") String search,
                          @Query("id") String id)
 
   @GET("/ssdservice/{version}/{type}/{source}/{source1}/{source2}")
