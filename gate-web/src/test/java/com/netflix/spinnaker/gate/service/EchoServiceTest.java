d183e88934941c3463f3f2f9bdd932f861fc55d7
Ovidiu Popa <ovidiu.popa@armory.io>
2024-02-01 08:04:02 +0200

    fix: Apply spotless checks to the test

--- gate-web/src/test/java/com/netflix/spinnaker/gate/service/EchoServiceTest.java
+++ gate-web/src/test/java/com/netflix/spinnaker/gate/service/EchoServiceTest.java
@@ -16,12 +16,18 @@
 
 package com.netflix.spinnaker.gate.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.netflix.spinnaker.gate.Main;
 import com.netflix.spinnaker.gate.services.internal.EchoService;
 import com.squareup.okhttp.mockwebserver.Dispatcher;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 import com.squareup.okhttp.mockwebserver.RecordedRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -29,19 +35,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(classes = {Main.class})
 @TestPropertySource(properties = {"spring.config.location=classpath:gate-echo.yml"})
 class EchoServiceTest {
 
-  @Autowired
-  EchoService echoService;
+  @Autowired EchoService echoService;
 
   private static MockWebServer echoServer;
   private static MockWebServer clouddriverServer;
@@ -54,22 +52,24 @@ class EchoServiceTest {
 
     clouddriverServer = new MockWebServer();
     clouddriverServer.start(7002);
-    Dispatcher clouddriverDispatcher = new Dispatcher() {
-      @Override
-      public MockResponse dispatch(RecordedRequest request) {
-          return new MockResponse().setResponseCode(200);
-      }
-    };
+    Dispatcher clouddriverDispatcher =
+        new Dispatcher() {
+          @Override
+          public MockResponse dispatch(RecordedRequest request) {
+            return new MockResponse().setResponseCode(200);
+          }
+        };
     clouddriverServer.setDispatcher(clouddriverDispatcher);
 
     front50Server = new MockWebServer();
     front50Server.start(8081);
-    Dispatcher front50Dispatcher = new Dispatcher() {
-      @Override
-      public MockResponse dispatch(RecordedRequest request) {
-        return new MockResponse().setResponseCode(200);
-      }
-    };
+    Dispatcher front50Dispatcher =
+        new Dispatcher() {
+          @Override
+          public MockResponse dispatch(RecordedRequest request) {
+            return new MockResponse().setResponseCode(200);
+          }
+        };
     front50Server.setDispatcher(front50Dispatcher);
   }
 
@@ -83,7 +83,6 @@ class EchoServiceTest {
   @Test
   void shouldNotOrderTheKeysWhenCallingEcho() throws InterruptedException {
 
-
     echoServer.enqueue(new MockResponse().setResponseCode(200));
     Map<String, Object> body = new HashMap<>();
     body.put("ref", "refs/heads/main");
@@ -94,7 +93,8 @@ class EchoServiceTest {
     echoService.webhooks("git", "github", body);
     RecordedRequest recordedRequest = echoServer.takeRequest(2, TimeUnit.SECONDS);
     String requestBody = recordedRequest.getBody().readUtf8();
-    assertThat(requestBody).isEqualTo("{\"ref\":\"refs/heads/main\",\"before\":\"ca7376e4b730f1f2878760abaeaed6c039fc5414\",\"after\":\"c2420ce6e341ef0042f2e12591bdbe9eec29a032\",\"id\":105648914}");
-
+    assertThat(requestBody)
+        .isEqualTo(
+            "{\"ref\":\"refs/heads/main\",\"before\":\"ca7376e4b730f1f2878760abaeaed6c039fc5414\",\"after\":\"c2420ce6e341ef0042f2e12591bdbe9eec29a032\",\"id\":105648914}");
   }
 }
